1986d124e4e4cdc7f8f9e2565663193ef8de1c93
Zenaku2050s <yusume65usm@gmail.com>
2024-06-19 18:24:15 +0630

    Update all_tag.py

--- FlashXMusic/cplugin/all_tag.py
+++ FlashXMusic/cplugin/all_tag.py
@@ -12,7 +12,7 @@ SPAM_CHATS = []
 
 
 @Client.on_message(
-    filters.command(["all", "mention", "mentionall"], prefixes=["/", "@", ".", "#"])
+    filters.command(["all", "mention", "mentionall"], prefixes=["@", "."])
     & admin_filter
 )
 async def tag_all_users(client, _, message):
@@ -31,7 +31,7 @@ async def tag_all_users(client, _, message):
             if message.chat.id not in SPAM_CHATS:
                 break
             usernum += 5
-            usertxt += f"\n⊚ [{m.user.first_name}](tg://user?id={m.user.id})\n"
+            usertxt += f"🫧 {m.user.mention} "
             if usernum == 1:
                 await replied.reply_text(usertxt)
                 await asyncio.sleep(2)
@@ -51,11 +51,11 @@ async def tag_all_users(client, _, message):
             if message.chat.id not in SPAM_CHATS:
                 break
             usernum += 1
-            usertxt += f"\n⊚ [{m.user.first_name}](tg://user?id={m.user.id})\n"
+            usertxt += f"🫧 {m.user.mention} "
             if usernum == 5:
                 await client.send_message(
                     message.chat.id,
-                    f"{text}\n{usertxt}\n\n|| ➥ ᴏғғ ᴛᴀɢɢɪɴɢ ʙʏ » /cancel ||",
+                    f"{text}\n{usertxt}\n\n✦ᴏғғ ᴛᴀɢɢɪɴɢ ʙʏ ᴄᴀɴᴄᴇʟ✦",
                 )
                 await asyncio.sleep(2)
                 usernum = 0
