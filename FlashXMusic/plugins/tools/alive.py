f4adf7348db7bb514048e4c2890a20e4866ede80
Zenaku2050s <yusume65usm@gmail.com>
2024-06-20 22:56:36 +0630

    Update alive.py

--- FlashXMusic/plugins/tools/alive.py
+++ FlashXMusic/plugins/tools/alive.py
@@ -15,20 +15,20 @@ async def start(client: Client, message: Message):
             [
                [
             InlineKeyboardButton(
-                text="☆ ʙʀᴀɴᴅᴇᴅ 💗 ", url=f"https://t.me/BRANDRDKING82"
+                text=" ʙʀᴀɴᴅᴇᴅ  ", url=f"https://t.me/NhoeKyaiteKaungLayy"
             ),
             InlineKeyboardButton(
-                text="☆ ꜱᴜᴘᴘᴏʀᴛ 💗", url=f"https://t.me/BRANDED_WORLD"
+                text=" ꜱᴜᴘᴘᴏʀᴛ ", url=f"https://t.me/seriousvs_version10"
             ),
         ],
                 [
             InlineKeyboardButton(
-                text="☆ ᴄʜᴀɴɴᴇʟ💗", url=f"https://t.me/BRANDRD_BOT"
+                text=" ᴄʜᴀɴɴᴇʟ", url=f"https://t.me/seriousvs_version20"
             ),
                 ],
                 [
                     InlineKeyboardButton(
-                        "✯ ᴄʟᴏsᴇ ✯", callback_data="close"
+                        " ᴄʟᴏsᴇ ", callback_data="close"
                     )
                 ],
             ]
