6141160aff1b6515aca5b4404bbd9c8422f7f50f
Zenaku2050s <yusume65usm@gmail.com>
2024-07-21 21:16:25 +0630

    Update stream.py

--- FlashXMusic/utils/stream/stream.py
+++ FlashXMusic/utils/stream/stream.py
@@ -480,3 +480,26 @@ async def stream(
             await mystic.delete()
 
 
+# Function to get thumbnail by video ID
+async def get_thumb(videoid):
+    try:
+        # Search for the video using video ID
+        query = f"https://www.youtube.com/watch?v={videoid}"
+        results = VideosSearch(query, limit=1)
+        for result in (await results.next())["result"]:
+            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
+        return thumbnail
+    except Exception as e:
+        return config.YOUTUBE_IMG_URL
+
+
+async def get_thumb(vidid):
+    try:
+        # Search for the video using video ID
+        query = f"https://www.youtube.com/watch?v={vidid}"
+        results = VideosSearch(query, limit=1)
+        for result in (await results.next())["result"]:
+            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
+        return thumbnail
+    except Exception as e:
+        return config.YOUTUBE_IMG_URL
