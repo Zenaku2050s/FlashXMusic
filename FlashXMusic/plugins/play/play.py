a8dfeac9375490cbb14c83433b1b4d9cb6ace734
Zenaku2050s <yusume65usm@gmail.com>
2024-07-21 13:19:41 +0630

    Update play.py

--- FlashXMusic/plugins/play/play.py
+++ FlashXMusic/plugins/play/play.py
@@ -662,9 +662,3 @@ async def slider_queries(client, CallbackQuery, _):
         return await CallbackQuery.edit_message_media(
             media=med, reply_markup=InlineKeyboardMarkup(buttons)
 )
-
-
-
-
-
-
