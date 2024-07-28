fc1942f3ac9e499d84359c2a01cf891705d3d414
Zenaku2050s <yusume65usm@gmail.com>
2024-07-28 19:31:14 +0630

    Update start.py

--- FlashXMusic/plugins/bot/start.py
+++ FlashXMusic/plugins/bot/start.py
@@ -7,7 +7,7 @@ from youtubesearchpython.__future__ import VideosSearch
 
 import config
 from config import BANNED_USERS
-from config.config import OWNER_ID
+from config import OWNER_ID
 from strings import get_command, get_string
 from FlashXMusic import Telegram, YouTube, app
 from FlashXMusic.misc import SUDOERS
